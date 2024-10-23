99b623cd969d79cdbde27da819790df4cea93703
jibinsajujoseph <155535585+jibinsajujoseph@users.noreply.github.com>
2024-10-23 20:00:52 +0530

    Update index.js - delete comments

--- index.js
+++ index.js
@@ -19,15 +19,6 @@ function clearLastDigit() {
     display.value = display.value.slice(0, -1);
 }
 
-// function calculate() {
-//     try {
-//         display.value = eval(display.value);
-//     }
-//     catch (error) {
-//         display.value = "Error";
-//     }
-// }   
-
 function calculate() {
     try {
         display.value = math.evaluate(display.value);
